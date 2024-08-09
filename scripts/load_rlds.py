644384af0dedc714152e1a752942e0129030367c
object814 <zhouhaoyu01@u.nus.edu>
2024-08-09 17:55:44 +0800

    update

--- scripts/load_rlds.py
+++ scripts/load_rlds.py
@@ -2,3 +2,266 @@
 script to run OpenVLA finetune dataset loading part for debugging
 '''
 
+"""
+finetune.py
+
+Simple script for parameter-efficient fine-tuning of OpenVLA models loaded through the HuggingFace AutoClasses, using
+HuggingFace PEFT library for low-rank adaptation (LoRA).
+
+Notes & Benchmarks:
+    - Requires PEFT (`pip install peft==0.11.1`)
+    - LoRA fine-tuning (see parameters below -- no quantization, LoRA rank = 32, target_modules = all-linear):
+        + One 48 GB GPU can fit a Batch Size of 12
+        + One 80 GB GPU can fit a Batch Size of 24
+
+Run with:
+    - [Single Node Multi-GPU (= $K) ]: torchrun --standalone --nnodes 1 --nproc-per-node $K vla-scripts/finetune.py
+    - [Override Config Values]: torchrun --standalone --nnodes 1 --nproc-per-node $K vla-scripts/finetune.py \
+                                    --data_root_dir <PATH/TO/RLDS/DATASETS/DIRECTORY> \
+                                    --dataset_name <DATASET_NAME> \
+                                    --run_root_dir <PATH/TO/LOGS/DIR> \
+                                    ...
+        
+    CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --standalone --nnodes 1 --nproc-per-node 4 vla-scripts/finetune.py \
+        --data_root_dir /data2/zhaoyu/LIBERO_rlds/ \
+        --dataset_name libero_spatial \
+        --run_root_dir /data2/zhaoyu/LIBERO_finetune/logs/libero_spatial \
+        --adapter_tmp_dir /data2/zhaoyu/LIBERO_finetune/checkpoints/libero_spatial
+"""
+
+import os
+os.environ['TRANSFORMERS_CACHE'] = '/data2/zhaoyu/huggingface_cache'
+from collections import deque
+from dataclasses import dataclass
+from pathlib import Path
+
+import draccus
+import torch
+import torch.distributed as dist
+import tqdm
+from accelerate import PartialState
+from peft import LoraConfig, PeftModel, get_peft_model, prepare_model_for_kbit_training
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.optim import AdamW
+from torch.utils.data import DataLoader
+from transformers import AutoModelForVision2Seq, AutoProcessor, BitsAndBytesConfig
+from transformers.modeling_outputs import CausalLMOutputWithPast
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname('__file__'), '../external/openvla')))
+import wandb
+from prismatic.models.backbones.llm.prompting import PurePromptBuilder, VicunaV15ChatPromptBuilder
+from prismatic.util.data_utils import PaddedCollatorForActionPrediction
+from prismatic.vla.action_tokenizer import ActionTokenizer
+from prismatic.vla.datasets import RLDSBatchTransform, RLDSDataset
+from prismatic.vla.datasets.rlds.utils.data_utils import save_dataset_statistics
+# Sane Defaults
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+# For NUS LinSLab server
+torch.set_num_threads(6)
+
+# # === Utilities ===
+# # fmt: off
+# def create_vision_transform(vla: nn.Module, input_size: int) -> Callable[[Image.Image], torch.Tensor]:
+#     """Gets image transform for the vision encoder."""
+#     data_cfg = timm.data.resolve_model_data_config(vla.vision_backbone)
+#     data_cfg["input_size"] = (3, input_size, input_size)
+#     return timm.data.create_transform(
+#         input_size=data_cfg["input_size"],
+#         interpolation=data_cfg["interpolation"],
+#         mean=data_cfg["mean"],
+#         std=data_cfg["std"],
+#         crop_pct=1.0,           # Set to 1.0 to disable cropping
+#         crop_mode="center",     # Default crop mode --> no-op when `crop_pct == 1.0`
+#         is_training=False,      # Disable image_aug when loading transform; handled by RLDS dataloader
+#     )
+#
+# # fmt: on
+
+
+@dataclass
+class FinetuneConfig:
+    # fmt: off
+    vla_path: str = "openvla/openvla-7b"                            # Path to OpenVLA model (on HuggingFace Hub)
+
+    # Directory Paths
+    data_root_dir: Path = Path("datasets/open-x-embodiment")        # Path to Open-X dataset directory
+    dataset_name: str = "droid_wipe"                                # Name of fine-tuning dataset (e.g., `droid_wipe`)
+    run_root_dir: Path = Path("runs")                               # Path to directory to store logs & checkpoints
+    adapter_tmp_dir: Path = Path("adapter-tmp")                     # Temporary directory for LoRA weights before fusing
+
+    # Fine-tuning Parameters
+    batch_size: int = 2                                             # Fine-tuning batch size
+    max_steps: int = 100000                                            # Max number of fine-tuning steps
+    save_steps: int = 100000                                           # Interval for checkpoint saving
+    learning_rate: float = 2e-5                                     # Fine-tuning learning rate
+    grad_accumulation_steps: int = 1                                # Gradient accumulation steps
+    image_aug: bool = False                                         # Whether to train with image augmentations
+    shuffle_buffer_size: int = 1_000                                # Dataloader shuffle buffer size (can reduce if OOM)
+
+    # LoRA Arguments
+    use_lora: bool = True                                           # Whether to use LoRA fine-tuning
+    lora_rank: int = 32                                             # Rank of LoRA weight matrix
+    lora_dropout: float = 0.0                                       # Dropout applied to LoRA weights
+    use_quantization: bool = False                                  # Whether to 4-bit quantize VLA for LoRA fine-tuning
+                                                                    #   => CAUTION: Reduces memory but hurts performance
+
+    # Tracking Parameters
+    wandb_project: str = "openvla"                                  # Name of W&B project to log to (use default!)
+    wandb_entity: str = "object814-national-university-of-singapore"                          # Name of entity to log under
+
+    # fmt: on
+
+
+@draccus.wrap()
+def finetune(cfg: FinetuneConfig) -> None:
+    batch_transform = RLDSBatchTransform(
+        action_tokenizer,
+        processor.tokenizer,
+        image_transform=processor.image_processor.apply_transform,
+        prompt_builder_fn=PurePromptBuilder if "v01" not in cfg.vla_path else VicunaV15ChatPromptBuilder,
+    )
+    vla_dataset = RLDSDataset(
+        cfg.data_root_dir,
+        cfg.dataset_name,
+        batch_transform,
+        resize_resolution=tuple(vla.module.config.image_sizes),
+        shuffle_buffer_size=cfg.shuffle_buffer_size,
+        image_aug=cfg.image_aug,
+    )
+
+    # [Important] Save Dataset Statistics =>> used to de-normalize actions for inference!
+    if distributed_state.is_main_process:
+        save_dataset_statistics(vla_dataset.dataset_statistics, run_dir)
+
+    # Create Collator and DataLoader
+    collator = PaddedCollatorForActionPrediction(
+        processor.tokenizer.model_max_length, processor.tokenizer.pad_token_id, padding_side="right"
+    )
+    dataloader = DataLoader(
+        vla_dataset,
+        batch_size=cfg.batch_size,
+        sampler=None,
+        collate_fn=collator,
+        num_workers=0,  # Important =>> Set to 0 if using RLDS; TFDS rolls its own parallelism!
+    )
+    print(len(dataloader))
+    input("PAUSE")
+
+    # Initialize Logging =>> W&B
+    if distributed_state.is_main_process:
+        wandb.init(entity=cfg.wandb_entity, project=cfg.wandb_project, name=f"ft+{exp_id}")
+
+    # Deque to store recent train metrics (used for computing smoothened metrics for gradient accumulation)
+    recent_losses = deque(maxlen=cfg.grad_accumulation_steps)
+    recent_action_accuracies = deque(maxlen=cfg.grad_accumulation_steps)
+    recent_l1_losses = deque(maxlen=cfg.grad_accumulation_steps)
+
+    # Initialize step counter
+    step_counter = 0
+
+    # Train!
+    with tqdm.tqdm(total=cfg.max_steps, leave=True) as progress:
+        vla.train()
+        optimizer.zero_grad()
+        print("entering for loop")
+        for batch_idx, batch in enumerate(dataloader):
+            with torch.autocast("cuda", dtype=torch.bfloat16):
+                output: CausalLMOutputWithPast = vla(
+                    input_ids=batch["input_ids"].to(device_id),
+                    attention_mask=batch["attention_mask"].to(device_id),
+                    pixel_values=batch["pixel_values"].to(torch.bfloat16).to(device_id),
+                    labels=batch["labels"],
+                )
+                loss = output.loss
+
+            # Normalize loss to account for gradient accumulation
+            normalized_loss = loss / cfg.grad_accumulation_steps
+
+            # Backward pass
+            normalized_loss.backward()
+
+            # Compute Accuracy and L1 Loss for Logging
+            action_logits = output.logits[:, vla.module.vision_backbone.featurizer.patch_embed.num_patches : -1]
+            action_preds = action_logits.argmax(dim=2)
+            action_gt = batch["labels"][:, 1:].to(action_preds.device)
+            mask = action_gt > action_tokenizer.action_token_begin_idx
+
+            # Compute Accuracy
+            correct_preds = (action_preds == action_gt) & mask
+            action_accuracy = correct_preds.sum().float() / mask.sum().float()
+
+            # Compute L1 Loss on Predicted (Continuous) Actions
+            continuous_actions_pred = torch.tensor(
+                action_tokenizer.decode_token_ids_to_actions(action_preds[mask].cpu().numpy())
+            )
+            continuous_actions_gt = torch.tensor(
+                action_tokenizer.decode_token_ids_to_actions(action_gt[mask].cpu().numpy())
+            )
+            action_l1_loss = torch.nn.functional.l1_loss(continuous_actions_pred, continuous_actions_gt)
+
+            # Store recent train metrics
+            recent_losses.append(loss.item())
+            recent_action_accuracies.append(action_accuracy.item())
+            recent_l1_losses.append(action_l1_loss.item())
+
+            # Compute gradient step index
+            gradient_step_idx = batch_idx // cfg.grad_accumulation_steps
+            print("Current gradient step index: ", gradient_step_idx)
+
+            # Compute smoothened train metrics
+            smoothened_loss = sum(recent_losses) / len(recent_losses)
+            smoothened_action_accuracy = sum(recent_action_accuracies) / len(recent_action_accuracies)
+            smoothened_l1_loss = sum(recent_l1_losses) / len(recent_l1_losses)
+
+            # Push Metrics to W&B (every 10 gradient steps)
+            if distributed_state.is_main_process and gradient_step_idx % 10 == 0:
+                wandb.log(
+                    {"train_loss": smoothened_loss, "action_accuracy": smoothened_action_accuracy, "l1_loss": smoothened_l1_loss}, step=gradient_step_idx
+                )
+
+            # Optimizer Step
+            if (batch_idx + 1) % cfg.grad_accumulation_steps == 0:
+                optimizer.step()
+                optimizer.zero_grad()
+                progress.update(cfg.grad_accumulation_steps)
+                step_counter += cfg.grad_accumulation_steps
+
+            # Save Model Checkpoint =>> by default, only keeps the latest checkpoint, continually overwriting it!
+            if gradient_step_idx > 0 and gradient_step_idx % cfg.save_steps == 0:
+                if distributed_state.is_main_process:
+                    print(f"Saving Model Checkpoint for Step {gradient_step_idx}")
+
+                    # If LoRA, we first save adapter weights, then merge into full model; otherwise, default save!
+                    save_dir = adapter_dir if cfg.use_lora else run_dir
+
+                    # Save Processor & Weights
+                    processor.save_pretrained(run_dir)
+                    vla.module.save_pretrained(save_dir)
+
+                # Wait for processor and adapter weights to be saved by main process
+                dist.barrier()
+
+                # Merge LoRA weights into model backbone for faster inference
+                #   =>> Note that merging is slow and can be done post-hoc to speed up training
+                if cfg.use_lora:
+                    base_vla = AutoModelForVision2Seq.from_pretrained(
+                        cfg.vla_path, torch_dtype=torch.bfloat16, low_cpu_mem_usage=True, trust_remote_code=True
+                    )
+                    merged_vla = PeftModel.from_pretrained(base_vla, adapter_dir)
+                    merged_vla = merged_vla.merge_and_unload()
+                    if distributed_state.is_main_process:
+                        merged_vla.save_pretrained(run_dir)
+
+                # Block on Main Process Checkpointing
+                dist.barrier()
+                
+                # Exit
+                if step_counter >= cfg.max_steps:
+                    break
+
+
+if __name__ == "__main__":
+    finetune()
